aaf17c14bc47f56fd42737943c98b092532e1d46
Unknown <marijn.grimberg@harvest.nl>
2018-07-12 18:29:50 +0200

    CF werkend gemaakt

--- src/main/java/com/harvest/verzekeren/VerzekerenBackendApplication.java
+++ src/main/java/com/harvest/verzekeren/VerzekerenBackendApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VerzekerenBackendApplication {
-
-	public static void main(String[] args) {
+public class VerzekerenBackendApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(VerzekerenBackendApplication.class, args);
 	}
 }
