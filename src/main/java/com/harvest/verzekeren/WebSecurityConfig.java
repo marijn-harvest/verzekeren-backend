c4bafd569c10740a4ade26bf05082db9bc181f78
Unknown <marijn.grimberg@harvest.nl>
2018-07-16 10:54:36 +0200

    security config cors aangezet voor frontend calls

--- src/main/java/com/harvest/verzekeren/WebSecurityConfig.java
+++ src/main/java/com/harvest/verzekeren/WebSecurityConfig.java
@@ -24,6 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 	protected void configure(HttpSecurity http) throws Exception
 	{
 		http
+			.cors().and()
 			.csrf().disable()
 			.authorizeRequests()
 			.anyRequest().authenticated()
