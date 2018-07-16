0f95e40a1f087bc3cef020be3c2982d00437fb72
Unknown <marijn.grimberg@harvest.nl>
2018-07-16 12:30:29 +0200

    login toegevoegd

---
+++ src/main/java/com/harvest/verzekeren/user/UserController.java
@@ -0,0 +1,16 @@
+package com.harvest.verzekeren.user;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController
+{
+	@GetMapping("/user")
+	public Principal getUser(Principal user)
+	{
+		return user;
+	}
+}
